96c48be1878d0cb644b54d9ac7a9d06ea428b979
Thaddeus M. Diede <thaddeusmd.dev@gmail.com>
2024-06-13 21:42:03 -0600

    Hoaxify project init Spring 3.2.6 Java 17

---
+++ hoaxify-backend/src/main/java/org/thadmd/hoaxify/HoaxifyApplication.java
@@ -0,0 +1,13 @@
+package org.thadmd.hoaxify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoaxifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoaxifyApplication.class, args);
+	}
+
+}
---
+++ hoaxify-backend/src/main/java/org/thadmd/hoaxify/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.thadmd.hoaxify;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HoaxifyApplication.class);
+	}
+
+}
